3021655886a7f8ccb4a94191622f5be625e17acd
Aravindan91 <124932613+Aravindan91@users.noreply.github.com>
2025-05-13 12:57:57 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,65 @@
+import mimetypes
+from flask import Flask, request , send_file
+# flask c le framework web pr gerer le server 
+# request , pr gerer les requette 
+# send_file pr envoyer le fichier ( comme le pixel.png ) 
+import logging
+# logging pr enregistrer les evenemnts 
+
+app = Flask(__name__)         
+## on vient de creer une sintance de flash , un gateau du moule                           
+
+#Pour la suite, nous devrons ajouter :
+# 1. Une route pour servir le pixel de tracking
+# 2. Une fonction pour enregistrer les ouvertures d'emails
+# 3. La configuration du logging
+
+# on config le loging pr voir les requette 
+# c comme si on cinf un carnet de note des requette 
+# logging.basicConfig , on prepare le carnet de note , avec : 
+#INFO : les info importante 
+# %(asctime)s : date et heure de la requette
+#levelsname , le type , le niv d'importance 
+# et enfin message , , le contenun du note 
+# on aura un truc du genre : 
+# 2024-01-20 14:30:45-INFO - Email ouvert par utilisateur@email.com
+logging.basicConfig(level=logging.INFO, format = '%(asctime)s-%(levelname)s - %(message)s')
+
+# on creer une route pr servir le pixel de tracking
+@app.route("/pixel.png")
+# le @ c comme un decoarateur , une etiquette , 
+#ici on dit si on accede dans le chemin pixl.png , on execute la f pixel() 
+def pixel():
+    # on recupere l'adresse mail de l'utilisateur
+    email = request.args.get("email")
+    #on save l'ouverture dans le log 
+    if email : 
+        logging.inf(f"le pixel tracked pr id : {email}")
+    # on enregistre l'ouverture de l email dans un ficher autre 
+        try : 
+            with open ("save trace email.txt", "a" , encoding="utf-8") as f : 
+                time = time.strftime("%Y-%m-%d %H:%M:%S")
+                f.write(f" {time} -email ouvert par : {email} \n")
+        except Exception as e : 
+            logging.error(f"erreur lors du save dans le fihcier , : {e}")
+    
+    # là on renvoi le pixel de tracking 
+    return send_file("pixel.png" , mimetypes='img/png')
+
+## là on va verifier si le server marche bien avc un msg smimple 
+
+@app.route("/")
+## pareille mais ici c'est dès l'entrée principale du server 
+def msg():
+    return "server de tracking online , ok !"
+
+if __name__ == "__main__":
+    app.run(debug=False)
+   # false c pr la production , y a pas tt les details , le juste necessaire 
+    #true c plus detaille , server qui change à chq modif , etc ( pas optimale pour la podution , pr donné au clienst ) 
+    
+# ## ATTENTION , ok le code est bon 
+# MAIS , ici le framework choisi , le server est de type flask , pr mo =n usage c'est ok 
+# mais dans le cas ou je le deploie / donne pur d'autre prsn , mieux vaut enivsager d'utiliser un sever plus robuste ( - Gunicorn
+# - uWSGI
+# - Waitress
---
+++ main.py
@@ -0,0 +1,136 @@
+import  smtplib
+import ssl
+import csv
+import time
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from jinja2 import Environment
+
+## les import les piece jointe 
+from email.mime.base import MIMEBase # pr les piece jointe 
+from email.mime.application import MIMEApplication # pr les pdf 
+from email.encoders import encode_base64# aussi important , c pr encoder ( à voir pk ) 
+
+
+fichier = 'mailtrack.csv'
+
+##ssl c'est pour la securité
+
+serveur_smtp = 'smtp.gmail.com'
+port_smtp = 465
+
+# info sur l email utilisé
+expediteur = 'aravindan.compte3@gmail.com'
+##mdp2 = '3b6pke2zA.232'
+
+##pr le 3 
+mdp = 'fxvg jouq rmqq klsd'
+
+# ##pr le mien 
+# mdp = 'afau bojf gyns fjlb'
+# dst = 'aravindan.compte@gmail.com'
+dst = ' arivarasi.detchanamourtty@gmail.com'
+# msg = """Subject: Test Gmail avec smtplib
+
+# Ceci est un message de test envoye via Gmail avec smtplib.
+# """
+
+
+contexte = ssl.create_default_context()
+
+contact=[]
+## ca va prendre les elm de chaqu collone , pr nos variable 
+try : 
+    with open(fichier,'r',encoding='utf-8') as fichier_csv:
+        lecteur_csv = csv.reader(fichier_csv)
+        next(lecteur_csv)  # Cette ligne permet de sauter la première ligne
+    ##c pr sauter la 1er ligne 
+        for i in lecteur_csv:
+            contact.append(i)
+            # print(i)
+            # print("\n")
+
+
+except FileNotFoundError:
+    print("fichier non trouvé , verifié son emplacement/nom")
+
+except Exception as e : 
+    print(f" y a une erreur inatentu : {e}")
+
+    
+try:
+    with smtplib.SMTP_SSL(serveur_smtp,port_smtp,context=contexte) as server:
+        server.login(expediteur,mdp)
+        
+        for i in contact:
+            # nom = i['nom']
+            # prenom= i['prenom']
+            # email = i['email']
+            # sx = i['sexe']
+            ## on peut pas ici car c pas un dico mais une lst , dc on doit acceder avc 
+
+            prenom = i[0]
+            nom = i[1]
+            sx = i[2]
+            email = i[3]
+
+            ## on creer le msg via MIME
+            message = MIMEMultipart('alternative')
+            # c'est un mot clée obligatoir, qui specifie ceux qu'on va use , alternative -> pr txt brut et html
+            message['From'] = expediteur    # Correct
+            message['To'] = email
+            message['Subject'] = "Candidature de stage"  # Correct
+
+            #cas txt brut 
+            # une approche avc jinja2 , c'est un peu pareille que avc les f , mais askip c mieux jinja2
+            text_template = Environment(autoescape=True).from_string("""
+            Bonjour {{sx}} {{nom}} {{prenom}},
+            J'espère que ce message vous trouve bien.
+            voici un petit message de test.
+
+            Cordialement,
+            Aravindan""")
+
+            msg = text_template.render(sx=sx, nom=nom, prenom=prenom)
+
+            with open('html-model.html','r',encoding='utf-8') as f :
+               template_contenu = f.read()
+               # ici on fait une photocpie de notre html 'f', 
+               html_template = Environment(autoescape=True).from_string(template_contenu)
+               # ici on fait une photocpie de notre html 'f', pn le met ds une envleoppe speciale MIMEAPP.. avc l
+           
+
+            # mtn on envoie la piece jointe 
+            try : 
+                with open('CV 7 aravindan detchanamourtty.pdf','rb') as f : 
+                    piece_jointe = MIMEApplication(f.read(),_subtype='pdf')# ici on fait une photocpie de notre pdf 'f' , pn le met ds une envleoppe speciale MIMEAPP.. avc l'etiquette subtype 
+                    piece_jointe.add_header('Content-Disposition','attachment',filename='CV 7 aravindan detchanamourtty.pdf')
+                    # le file name renomme le fichier pdf lors de l'nevoi ( renomation est que sur le fichier recu par client )
+                   ## les mot clée contant-disposition, attachment, filename, sont des mot clé obligatoir/par defaut 
+                   #ca dit comment le use , le presenter ,                     # c pr dire que c une piece jointe
+                    message.attach(piece_jointe)
+
+            except FileNotFoundError : 
+                print(" fichier introuvable ")  
+            except Exception as e : 
+                print(f" une erreur intauentud lors de la piece jointe : {e}")
+
+            # donner les var au template 
+            html_final = html_template.render(sx=sx, nom=nom, prenom=prenom)
+
+            # Attacher les deux versions
+            part1 = MIMEText(msg,'plain')
+            part2 = MIMEText(html_final,'html')
+            message.attach(part1)
+            message.attach(part2)
+
+            server.send_message(message)
+            print(f" email envoyé avc succé pour le mail : {email}")
+            ##time.sleep(0.1)
+        server.quit()  # Ajout de server.quit()
+
+       
+
+except Exception as e :
+    print(f" y a une erreur lors de l'nvoie : {e}")
+print(" tout est terminé ")
